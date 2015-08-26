b04fc77d2daa98ddf94f7e86d8a88714e7c05957
Tischenko Sergiy <sytischenko@gmail.com>
2015-08-26 14:58:28 +0300

    solution in 1 row added

--- anagram.py
+++ anagram.py
@@ -1,5 +1,5 @@
 def anagramSolution(s1,s2):
 	if {i:s1.count(i) for i in s1}=={i:s2.count(i) for i in s2}:
 		return True
-		
+# in 1 row - anagramCheking = lambda s1,s2:{i:s1.count(i) for i in s1}=={i:s2.count(i) for i in s2}		
 anagramSolution('python','typhon')=> True or None
