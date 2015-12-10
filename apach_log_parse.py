6d215b308a9772fc5efc960da8272cef78ff0e24
Sergiy Tischenko <s.tischenko@samsung.com>
2015-12-10 19:51:00 +0200

    add c testing

--- apach_log_parse.py
+++ apach_log_parse.py
@@ -6,7 +6,7 @@ time.clock()
 127.0.0.1 - - [27/Sep/2015:06:30:55 +0300] "GET /server-status HTTP/1.1" 200 7217 "-" "Python-urllib/2.7"
 127.0.0.1 - - [27/Sep/2015:06:30:55 +0300] "POST /server-status HTTP/1.1" --- "-" "Python-urllib/2.7"
 106.125.52.69 - - [27/Sep/2015:06:30:58 +0300] "GET /ua.archive.ubuntu.com/12_04/dists/precise/Release.gpg HTTP/1.1" 304 187 "-" "Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.16)
-""""
+"""
 # create template for search in every log line, use re.search.
 # (\d{3}) - create one group.
 # Ex.("[GET|POST]).*?(HTTP.*?)"\s{1,5}(\d{3})? - will find 3 groups ("GET or "POST),(HTTP/1.1),(200)
@@ -16,7 +16,7 @@ template = re.compile('"[GET|POST].*?HTTP.*?"\s{1,5}(\d{3})?')
 
 with open("access.log.1") as f:
     for line in f.readlines()[-3:]:
-        key = template.search(line).groups()[0]
+        key = template.search(line).group()
         if key:
             result[key]=result.setdefault(key,0)+1
         
